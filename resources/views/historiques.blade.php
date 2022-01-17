8756ee8926b1675e361121f26af8f3de5698a0b6
sly987 <76872198+sly987@users.noreply.github.com>
2022-01-17 16:01:05 +0100

    historique et création notif

--- resources/views/historiques.blade.php
+++ resources/views/historiques.blade.php
@@ -9,14 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                @foreach($annees as $annee) 
-{{ $annee->id }}
-<a href="{{ route('historique.showing' , ['id' => $annee->id]) }}">Valider</a>
-<br>
-                    @endforeach
-
+                {!! Form::open(['route' => ['historique.showing'], 'method' => 'get']) !!}
+                    <select name='annee' id='annee'>
+                        @foreach($annees as $annee) 
+                            <option value={{ $annee->id }}>{{$annee->id}}</option>
+                        @endforeach
+                    </select>
+                    <input type='submit'>valider</input>
                 
-                </form>
+                    {!! Form::close() !!}
             </div>
             </div>
         </div>
