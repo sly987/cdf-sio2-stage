8c21021655a7686fbe7e5c8b0e5d48d79f895200
sly987 <76872198+sly987@users.noreply.github.com>
2022-01-18 09:36:47 +0100

    password dans user migration

--- database/migrations/2022_01_17_080056_alter_users.php
+++ database/migrations/2022_01_17_080056_alter_users.php
@@ -14,8 +14,7 @@ class AlterUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('prenom');
-            $table->string('password');
+            $table->string('prenom');          
             $table->boolean('admin')->default(0);
             $table->renameColumn('name', 'nom');
 
