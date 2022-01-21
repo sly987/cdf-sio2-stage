c0a298bb1d6dbfe7b1c83c3defb31d3fe5333a22
sly987 <76872198+sly987@users.noreply.github.com>
2022-01-21 15:58:23 +0100

    Create SendEmail.php

---
+++ app/Console/Commands/SendEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\test;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:sendMail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Mail::to('test@test.fr')->send(new test());;
+    }
+}
