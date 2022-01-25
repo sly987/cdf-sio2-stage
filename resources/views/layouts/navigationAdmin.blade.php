de48b7f22940cfa6e26e83b2928261170bfa2333
sly987 <76872198+sly987@users.noreply.github.com>
2022-01-25 15:13:16 +0100

    bouton retour

--- resources/views/layouts/navigationAdmin.blade.php
+++ resources/views/layouts/navigationAdmin.blade.php
@@ -21,9 +21,15 @@
                         <h4>Liste professeurs</h4>
                     </x-nav-link>
                 </div>
+               
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('reglage')">
                         <h4>{{$anneeDebut+$anneeChoisie-1}}/{{$anneeDebut+$anneeChoisie}} </h4>
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ url()->previous() }}">
+                            <h4>Retour</h4>
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/layouts/navigationUser.blade.php
+++ resources/views/layouts/navigationUser.blade.php
@@ -23,6 +23,11 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ url()->previous() }}">
+                            <h4>Retour</h4>
+                        </x-nav-link>
+                    </div>
             </div>
 
             <!-- Settings Dropdown -->
