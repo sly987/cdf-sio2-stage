3d236ff80ef4baaa807088a312231b8208b01769
sly987 <76872198+sly987@users.noreply.github.com>
2022-01-31 10:55:40 +0100

    listPM mise à jour

--- resources/views/admin/listPM.blade.php
+++ resources/views/admin/listPM.blade.php
@@ -25,47 +25,74 @@
                     </div>
                 </div>
                 <div>
-                    <table>
-                        <tr>
-                            <td width='30%'>
-                                nom
-                            </td width='30%'>
-                            <td>
-                                prenom
-                            </td>
-                            <td width='30%'>
-                                statut de la fiche
-                            </td>
-                        </tr>
-                        @if(isset($users))
-                            @foreach ($users as $user )
-                                <tr>
-                                    <td>
-                                        {{$user->nom}}
-                                    </td>
-                                    <td>
-                                        {{$user->prenom}}
-                                    </td>
-                                    @foreach ($user->fiches as $fiche)
+                    <table cellpadding="10" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th width='25%'>
+                                    nom
+                                </th width='25%'>
+                                <th>
+                                    prenom
+                                </th>
+                                <th width='25%'>
+                                    Fiche
+                                </th>
+                                <th width='25%'>
+                                    confirmé?
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(isset($users))
+                                @foreach ($users as $user )
+                                    <tr>
+                                        <td>
+                                            {{$user->nom}}
+                                        </td>
+                                        <td>
+                                            {{$user->prenom}}
+                                        </td>
+                                        @foreach ($user->fiches as $fiche)
 
-                                        @if ($fiche->mois_id==$mois_id[0]->id)
-                                            @if($fiche->envoye == 1 AND $fiche->chemin_fiche != NULL)
-                                                <td><a href="{{ route('file.download', $fiche->id) }}">&#x1F4E5</a></td>
-                                                @if($fiche->confirme == 0)
-                                                   <td> <a href="{{ route('file.destroy', $fiche->id) }}">&#x1F5D1</a></td>
-                                                @endif
-                                            @else
-                                                @if($fiche->mois->mois <= $moisEnCours)
-                                                    <td>&#x1F553 Retard</td>
-                                                @else
-                                                    <td>&#x274C</td>
-                                                @endif
-                                            @endif 
-                                        @endif
-                                    @endforeach
-                                </tr>
-                            @endforeach
-                        @endif
+                                            @if ($fiche->mois_id==$mois_id[0]->id)
+                                                <td> 
+                                                                
+                                                    @if($fiche->envoye == 1 AND $fiche->chemin_fiche != NULL)
+                                                        <a href="{{ route('file.download', $fiche->id) }}">&#x1F4E5</a>
+                                                        @if($fiche->confirme == 0)
+                                                            <a href="{{ route('file.destroy', $fiche->id) }}">&#x1F5D1</a>
+                                                        @endif
+                                                    @else
+                                                        
+                                                        @if($fiche->mois->annee->annee < date ('Y'))
+                                                            <p>&#x1F553 Retard</p>
+                                                        @else
+                                                            @if($fiche->mois->mois < $moisEnCours AND $fiche->mois->annee->annee == date('Y'))
+                                                            {{-- $fiche->mois->actif == 1 --}}
+                                                                <p>&#x1F553 Retard</p>
+                                                            @else
+                                                                <p>&#x1F512</p>
+                                                            @endif
+                                                        @endif
+                                                    @endif
+                                                </td>
+                                                <td align="center">
+                                                    @if($fiche->confirme == 1)
+                                                        <p>&#x2705</p>
+                                                    @else
+                                                        @if($fiche->envoye == 1 AND $fiche->chemin_fiche != NULL)
+                                                            <a href="{{ route('admin.confirmed', $fiche->id) }}">&#x274C</a>
+                                                        @else
+                                                            <p>&#x1F512</p>	
+                                                        @endif
+                                                    @endif
+                                                </td>
+                                            @endif
+                                        @endforeach
+                                    </tr>
+                                @endforeach
+                            @endif
+                        </tbody>
                     </table>
                 </div>
             </div>
